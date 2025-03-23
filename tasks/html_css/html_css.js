90a9438790bad87a76c167bb969149f7276bc6e5
sangeeth7420 <sangeethreddy3742@gmail.com>
2025-03-23 16:10:49 -0500

    Update html_css.js

--- tasks/html_css/html_css.js
+++ tasks/html_css/html_css.js
@@ -1,4 +1,20 @@
+// function openTask(folderName) {
+//     const iframe = document.getElementById("header_div");
+//     const display = document.getElementById("display-area");
+  
+//     iframe.style.display = "block";
+//     display.style.display = "none";
+//     iframe.src = `tasks/${folderName}/dashboard.html`;
+  
+//     // Optionally set title manually
+//     document.getElementById("task-frame").textContent = folderName.replace(/_/g, ' ').replace(/\b\w/g, l => l.toUpperCase());
+//   }
 
-//function openPage(page) {
-  //  window.location.href = "https://sangeeth7420.github.io/Dynamic_project/tasks/html_css/" + page;
-//}
+function openPage(page) {
+    fetch(page)
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('content_div').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading the page:', error));
+}
